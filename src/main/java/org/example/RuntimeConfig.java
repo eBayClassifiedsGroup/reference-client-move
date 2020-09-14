b412bb0094aa401d538ee9adcd3078adf691f917
Philipp Bartsch <pbartsch@team.mobile.de>
2020-09-14 09:34:29 +0200

    place credentials in a config that is not tracked with git

--- src/main/java/org/example/RuntimeConfig.java
+++ src/main/java/org/example/RuntimeConfig.java
@@ -1,25 +1,34 @@
 package org.example;
 
 import java.io.IOException;
+import java.net.URL;
 import java.util.Properties;
 
 public class RuntimeConfig {
 
-    private final String userName;
-    private final String password;
-    private final String partnerId;
-    private final String moveBaseUrl;
+    private String userName;
+    private String password;
+    private String partnerId;
+    private String moveBaseUrl;
 
     public RuntimeConfig() {
-        try {
-            Properties prop = new Properties();
-            prop.load(RuntimeConfig.class.getClassLoader().getResourceAsStream("config.properties"));
-            userName = prop.getProperty("auth.user");
-            password = prop.getProperty("auth.password");
-            partnerId = prop.getProperty("partnerId");
-            moveBaseUrl = prop.getProperty("moveBaseUrl");
-        } catch (IOException e) {
-            throw new IllegalStateException("Failed to read runtime props.", e);
+        ingestProperties("config.properties");
+        ingestProperties("untracked-config.properties");
+    }
+
+    private void ingestProperties(String s) {
+        URL resource = RuntimeConfig.class.getClassLoader().getResource(s);
+        if (resource != null) {
+            try {
+                Properties prop = new Properties();
+                prop.load(RuntimeConfig.class.getClassLoader().getResourceAsStream(s));
+                userName = prop.getProperty("auth.user");
+                password = prop.getProperty("auth.password");
+                partnerId = prop.getProperty("partnerId");
+                moveBaseUrl = prop.getProperty("moveBaseUrl");
+            } catch (IOException e) {
+                throw new IllegalStateException("Failed to read runtime props.", e);
+            }
         }
     }
 
